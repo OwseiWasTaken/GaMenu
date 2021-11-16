6587be8384b856a0622e93136c82de9747e56ea7
owsei <owseiwastaken@gmail.com>
2021-11-15 21:40:29 -0300

    calculator panic system

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,20 @@
 from lib import *
 from math import sqrt
+import sys
 
-#TODO panic &{}() system
+errors = {
+	1:"more deffinitions than possible!",
+	2:"Var Reference [%s] Not Found In Memory [%s]",
+	3:"can't compute operation \"%s\"",
+	4:"can't find external Reference \"%s\"",
+}
+
+def panic(ecode, *args):
+	msg = errors[ecode]
+	if args:
+		msg = msg % args
+	sys.stderr.write(msg+'\n')
+	sys.exit(ecode)
 
 def main():
 	#Intro:
@@ -13,10 +26,12 @@ def main():
 	mem = {}
 	while True:
 		op = input('>')
+		if op in ["exit", "break", "quit"]:break
 		memname = "\x00" # \x00 not "inputable"
 
 		if '=' in op: # mem def
-			assert op.count('=') == 1, "more deffinitions than possible!"
+			if op.count('=') != 1:
+				panic(1)
 			memname, op = op.split('=')
 			op = op.strip()
 			memname = memname.strip()
@@ -25,7 +40,8 @@ def main():
 			op = SplitBracket(op, '{')
 			for opi in range(len(op)): # "3{x}5" -> ['3', 'x', '5']
 				if opi%2: # get every OTHER var (if in {} index will be even)
-					assert op[opi] in mem.keys(), "Var Reference [%s] Not Found In Memory [%s]" % (op[opi], (mem if len(mem) < 20 else "mem size too big"))
+					if not op[opi] in mem.keys():
+						panic(2, (op[opi], (mem if len(mem) < 20 else "mem size too big")))
 					if op[opi] in mem.keys():
 						op[opi] = mem[op[opi]]
 
@@ -35,9 +51,9 @@ def main():
 			try:
 				evldop = eval(op)
 			except SyntaxError:
-				assert 0, "can't compute operation \"%s\"" % op
+				panic(3, op)
 		except NameError:
-			assert 0, "can't find external Reference \"%s\"" % op
+			panic(4, op)
 		if memname == "\x00":
 			print(evldop)
 		else:
