4ecce61b98ffce046e0648ac8f6341d4be9de852
owsei <owseiwastaken@gmail.com>
2021-11-17 09:34:32 -0300

    main.py.main

--- forca.py
+++ forca.py
@@ -1,13 +1,16 @@
 #Intro:
-print("*************************************")
-print("			 Welcome to Hangman			")
-print("*************************************")
-print()
+def main()
+	print("*************************************")
+	print("			 Welcome to Hangman			")
+	print("*************************************")
+	print()
 
-ContiuarJogo = "y"
-Score = 0
-Tentativas = 0
+	ContiuarJogo = "y"
+	Score = 0
+	Tentativas = 0
 
-print()
-print(f"Sua pontuação foi de: {Score}! Com {Tentativas} tentativas")
+	print()
+	print(f"Sua pontuação foi de: {Score}! Com {Tentativas} tentativas")
 
+if __name__ == "__main__":
+	main()
--- main.py
+++ main.py
@@ -1,57 +1,62 @@
 #! /bin/python3.10
 
 from lib import *
-import TestZone
-import RpgSheet
-import calculadora
-import adivinhacao
-#import forca
+from TestZone import main as TestZone
+from RpgSheet import main as RpgSheet
+from calculadora import main as calculadora
+from adivinhacao import main as adivinhacao
+from forca import main as forca
 
-print("********************************************")
-print("*                   Menu                   *")
-print("********************************************")
-print()
-
+menu = """
+********************************************
+*				   Menu					   *
+********************************************
+"""
 ListOfOptions = """
 Which program do your want to use?
 1. Hangman [Under development]
 2. Guess the Number
 3. Calculator
 4. RPG Sheet Editor
-5. TestZone
+5. TestZone [Under development]
 6. Exit Prograram
 
 
 Write your choice:
-"""[1:-1] # [...] cus \n at start and end
-class OP:
-        HANGMAN = 1
-        GUESS = 2
-        CALC = 3
-        RPG = 4
-        TEST = 5
-        EXIT = 6
+"""
 
-while True:
-        clear()
-        print(ListOfOptions)
-        Doing = GetIn(GetInt, range(1, 8), '>')
-        match Doing:
-                case OP.HANGMAN:
-                        assert False, "Under development"
-                case OP.GUESS:
-                        print("Now running 'Guess the Number'\n")
-                        adivinhacao.main()
-                case OP.CALC:
-                        print("Now running 'Calculator'\n")
-                        calculadora.main()
-                case OP.RPG:
-                        print("Now running 'RPG Sheet Editor'\n")
-                        RpgSheet.main()
-                case OP.TEST:
-                        print("Now running 'TestZone'\n")
-                        TestZone.main()
-                case OP.EXIT:
-                        print("Exiting...")
-                        break
+class OP(IntEnum):
+	HANGMAN = auto()
+	GUESS = auto()
+	CALC = auto()
+	RPG = auto()
+	TEST = auto()
+	EXIT = auto()
+	COUNT = auto()
 
+def main():
+	while True:
+		clear()
+		print(ListOfOptions)
+		Doing = GetIn(GetInt, range(1, OP.COUNT), '>')
+		match Doing:
+			case OP.HANGMAN:
+				assert 0, "Under development"
+				forca()
+			case OP.GUESS:
+				print("Now running 'Guess the Number'\n")
+				adivinhacao()
+			case OP.CALC:
+				print("Now running 'Calculator'\n")
+				calculadora()
+			case OP.RPG:
+				print("Now running 'RPG Sheet Editor'\n")
+				RpgSheet()
+			case OP.TEST:
+				print("Now running 'TestZone'\n")
+				TestZone()
+			case OP.EXIT:
+				print("Exiting...")
+				break
+
+if __name__ == "__main__":main()
